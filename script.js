7acdc17c3d28a2c3ba4a6bc1911a77a1421099d0
HCQPersonal <personalz.hcq@gmail.com>
2020-05-10 12:59:18 -0700

    feat: add initial submit event lister for form

--- script.js
+++ script.js
@@ -1,45 +1,49 @@
-let formModal = document.querySelector('.modal');
+const formModal = document.querySelector('.modal');
+const form = document.querySelector('.modal__form');
 const formOpen = document.querySelector('.profile__edit-button');
 const formClose = document.querySelector('.modal__close');
-let formSave = document.querySelector('.modal__save-btn');
+// const formSave = document.querySelector('.modal__save-btn');
+
+let userName = document.querySelector('.profile__info_name');
+let userJob = document.querySelector('.profile__info_description');
+let nameInput = document.querySelector('.modal__input_name');
+let jobInput = document.querySelector('.modal__input_description');
+
 
 function formOpenHandler() {
     formModal.style.display="block";
-}
 
+    nameInput.value = userName.textContent;
+    jobInput.value = userJob.textContent;
+}
 formOpen.addEventListener('click', formOpenHandler);
 
 function formCloseHandler() {
     formModal.style.display="none";
 }
-
 formClose.addEventListener('click', formCloseHandler);
 
-// function formSubmitHandler(evt) {
-//     evt.preventDefault();
 
-//     let nameInput = document.querySelector('.modal__name');
-//     let jobInput = document.querySelector('.modal__description');
+form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
 
-//     // Get the values of each field from the corresponding value property
-//     if (nameInput.length > 0) {
-//         nameInput.setAttribute('nameInput', '');
-//     } else {
-//         formElement.setat
-//     }
+// userName.textContent = 
 
-//     // Select elements where the field values will be entered
+//     info.insertHTML('beforeend', `
+//     <div class="profile__info">
+//         <h1 class="profile__info_name">${userName.value}</h1>
+//         <p class="profile__info_description">${userJOob.value}</p>
+//     </div>
+// `);
 
-//     // Insert new values using the textContent property of the querySelector() method
-//     let profileName = document.querySelector('profile__name').textContent;
-//     let profileJob = document.querySelector('profile__description').textContent;
-
-//     nameInput.value = "";
-//     jobInput.value = "";
+    // userName.value = "";
+    // userJob.value = "";
 
 //     renderAdded();
-// }
+    formCloseHandler();
+})
+
+// make modal background opaque & fixed
+
 
 // formSave.addEventListener('submit', formSubmitHandler);
-
-// renderAdded();
