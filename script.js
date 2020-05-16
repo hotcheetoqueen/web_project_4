f9505f009985ec224c0a0b4937ce260bc46bcca9
HCQPersonal <personalz.hcq@gmail.com>
2020-05-15 21:55:19 -0700

    hotfix/update javascript to avoid direct calls to styles and css

--- script.js
+++ script.js
@@ -10,25 +10,16 @@ const nameInput = document.querySelector('.modal__input_name');
 const jobInput = document.querySelector('.modal__input_description');
 
 
-function formOpenHandler() {
-    overlay.style.opacity = ".5";
-
-    if (formModal.style.display === "block") {
-        formCloseHandler();
-      } else {
-        formModal.style.display = "block";
-      }
+const toggleHandler = () => {
+    overlay.classList.toggle('overlay_visible');
+    formModal.classList.toggle('modal_visible');
 
     nameInput.value = userName.textContent;
     jobInput.value = userJob.textContent;
+}
 
-} formOpen.addEventListener('click', formOpenHandler);
-
-
-function formCloseHandler() {
-    formModal.style.display="none";
-    overlay.style.opacity = "1";
-} formClose.addEventListener('click', formCloseHandler);
+formOpen.addEventListener('click', toggleHandler);
+formClose.addEventListener('click', toggleHandler);
 
 
 form.addEventListener('submit', (evt) => {
@@ -37,5 +28,5 @@ form.addEventListener('submit', (evt) => {
     userName.textContent = nameInput.value;
     userJob.textContent = jobInput.value;
 
-    formCloseHandler();
+    toggleHandler();
 });
