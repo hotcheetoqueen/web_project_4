a9a4562b186fcc0d153635819f1595f5d7024d1e
HCQPersonal <personalz.hcq@gmail.com>
2020-06-14 11:52:33 -0700

    style/cleans up error modal styles

--- validate.js
+++ validate.js
@@ -3,7 +3,7 @@ const validateObject = {
   inputSelector: '.modal__input',
   submitButtonSelector: '.modal__save-btn',
   invalidInputClass: 'modal__input_disabled',
-  inactiveButtonClass: 'modal__save-btn_disabled',
+  inactivebuttonElemClass: 'modal__save-btn_disabled',
   inputErrorClass: 'modal__input_error',
   errorClass: 'modal__input_error_active'
 };
@@ -15,56 +15,59 @@ const validateObject = {
 function showInputError(formElement, input, errorMessage) {
     const error = formElement.querySelector(`#${input.id}-error`);
 
-    error.classList.add(formElement.errorClass);
-    input.classList.add(formElement.inputErrorClass);
+    error.classList.add(validateObject.errorClass);
+    input.classList.add(validateObject.inputErrorClass);
     error.textContent = errorMessage;
 }
 
 function hideInputError(formElement, input) {
   const error = formElement.querySelector(`#${input.id}-error`);
 
-    error.classList.remove(formElement.errorClass);
-    input.classList.remove(formElement.inputErrorClass);
+    error.classList.remove(validateObject.errorClass);
+    input.classList.remove(validateObject.inputErrorClass);
     error.textContent = "";
 }
 
 // Check input validity
 function checkInputValidity(formElement, input) {
-    if (input.validity.valid) {
-      hideInputError(formElement, input);
-    } else {
+    if (!input.validity.valid) {
       showInputError(formElement, input, input.validationMessage);
+    } else {
+      hideInputError(formElement, input);
     }
 }
-
-const button = Array.from(document.querySelectorAll(validateObject.submitButtonSelector));
-const inputList = Array.from(document.querySelectorAll(validateObject.inputSelector));
-  
-  const isInvalid = inputList.some((input) => {
+ 
+const isInvalid = (inputList) => {
+  return inputList.some((input) => {
     return !input.validity.valid;
-  })
+  });
+};
 
-// Disable button
-  function toggleButton(inputList, button) {
-    if (isInvalid(inputList)) {
-      button.classList.add(validateObject.inactiveButtonClass);
-      button.disabled = true;
-    } else {
-      button.classList.remove(validateObject.inactiveButtonClass);
-      button.disabled = false;
-    }
-  };
+// Disable buttonElem
+const togglebuttonElem = (inputList, buttonElem) => {
+  if (isInvalid(inputList)) {
+    buttonElem.classList.add(validateObject.inactivebuttonElemClass);
+    buttonElem.disabled = true;
+  } else {
+    buttonElem.classList.remove(validateObject.inactivebuttonElemClass);
+    buttonElem.disabled = false;
+  }
+};
 
-  // Validation listeners
-  function setEventListeners(formElement) {
+// Validation listeners
+function setEventListeners(formElement) {
+  const inputList = Array.from(document.querySelectorAll(validateObject.inputSelector));
+  const buttonElem = document.querySelectorAll(validateObject.submitButtonSelector);
 
-    inputList.forEach((input) => {
-        input.addEventListener('input',  () => {
-          checkInputValidity(formElement, input);
-          toggleButton(inputList, button);
-        });
+  // togglebuttonElem(inputList, buttonElem);
+
+  inputList.forEach((input) => {
+      input.addEventListener('input',  () => {
+        checkInputValidity(formElement, input);
+        togglebuttonElem(inputList, buttonElem);
       });
-    };
+    });
+  };
 
 // Overall validation
 function enableValidation(form) {
@@ -72,9 +75,16 @@ function enableValidation(form) {
     formList.forEach((formElement) => {
         formElement.addEventListener('submit', (evt) => {
             evt.preventDefault();
-        })
+        });
         setEventListeners(formElement);
-      })
-};
+      });
+}
+
+//         const fieldsetList = Array.from(formElement.querySelectorAll(".form__fields"));
+//         fieldsetList.forEach((fieldset) => {
+//           setEventListeners(fieldset);
+//         });
+//       });
+// };
 
 enableValidation(validateObject);
