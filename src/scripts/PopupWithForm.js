03997b802f2ab4743d9a112569c153eaa9c93d35
HCQPersonal <personalz.hcq@gmail.com>
2020-07-11 11:50:31 -0700

    refactor/create PopupWithForm.js

--- src/scripts/PopupWithForm.js
+++ src/scripts/PopupWithForm.js
@@ -1,7 +1,32 @@
-// import Popup from './popup.js';
+import Popup from './popup.js';
 
-// class PopupWithForm extends Popup {
+export default class PopupWithForm extends Popup {
+    constructor(popupSelector, handleFormSubmit) {
+        super(popupSelector);
 
-// }
+        this._handleFormSubmit = handleFormSubmit;
 
-// export default PopupWithForm;
+    }
+
+    _getInputValues() {
+        this._values = {};
+        this._inputList.forEach((input) => {
+            this._values[input.name] = input.value;
+        });
+        
+        return this._values;
+    }
+
+    setEventListeners() {
+        super.setEventListeners();
+        this._popupElement.addEventListener("submit", (evt) => {
+            evt.preventDefault();
+            this._handleFormSubmit(this._getInputValues());
+        });
+    }
+
+    close() {
+        this._form.reset();
+        super.close();
+      }
+}
