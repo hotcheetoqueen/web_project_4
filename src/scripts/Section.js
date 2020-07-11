ff41cd707082a808c4f3a703550891a0b5ab32a5
HCQPersonal <personalz.hcq@gmail.com>
2020-07-11 11:28:34 -0700

    refactor/create Section.js

--- src/scripts/Section.js
+++ src/scripts/Section.js
@@ -1,12 +1,17 @@
-class Section {
-    constructor( { items, renderer }, cardContainer) {
-            // cardContainer = document
-            // .querySelector
-            // .('')
-            // .content
+export default class Section {
+    constructor({ items, renderer }, cardContainer) {
+        this._array = items;
+        this._renderer = renderer;
+        this._container = document.querySelector(cardContainer);
+    }
+
+    renderer() {
+        this._array.forEach((item) => {
+            this._renderer(item);
+        });
+    }
                 
-            // addItem() {
-            //     this._element.add();
-            // }
-        }
+    addItem(element) {
+        this._container.prepend(element);
+    }
 }
