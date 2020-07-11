1a08187ceb1185b05d7b7c99d3781b4f59c84113
HCQPersonal <personalz.hcq@gmail.com>
2020-07-11 11:38:15 -0700

    refactor/create Popup.js

--- src/scripts/Popup.js
+++ src/scripts/Popup.js
@@ -1,15 +1,19 @@
-class Popup {
+export default class Popup {
     constructor(popupSelector) {
-        // this._popupSelect = popupSelector;
+        this._popupSelect = popupSelector;
         this._popupElement = document.querySelector(popupSelector);
     }
         open() {
-            this._popupElement.classList.add('modal__open');
+            this._popupElement.classList.add('modal_visible');
+            this._popupOverlay.classList.add('overlay_visible');
+            
             document.addEventListener('keyup', this._handleEscClose);
         }
 
         close() {
-            this._popupElement.classList.remove('modal__open');
+            this._popupElement.classList.remove('modal_visible');
+            this._popupOverlay.classList.remove('overlay_visible');
+
             document.addEventListener('keyup', this._handleEscClose);
         }
 
@@ -21,11 +25,9 @@ class Popup {
 
         setEventListeners() {
             this._popupElement.addEventListener('click', (evt) => {
-                if(evt.target.classList.contains('popup__close')) {
+                if(evt.target.classList.contains('modal__close')) {
                     this.close();
                 }
             })
         }
 }
-
-export default Popup;
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,6 @@
 import FormValidator from './FormValidator.js';
 import Card from './Card.js';
-// import Popup from './Popup.js';
+import Popup from './Popup.js';
 import PopupWithImage from './PopupWithImage.js';
 import { popUp, overlay, togglePopHandler, popUpCreator } from './utils.js';
 import "../pages/index.css";
