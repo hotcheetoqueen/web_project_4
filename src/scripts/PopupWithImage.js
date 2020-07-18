224cd1ce1809d36c0f6ace1059b222b15eb89624
HCQPersonal <personalz.hcq@gmail.com>
2020-07-18 13:30:17 -0700

    hotfix/proper import of Popup.js into WithImage/Form classes

--- src/scripts/PopupWithImage.js
+++ src/scripts/PopupWithImage.js
@@ -1,12 +1,12 @@
-import Popup from './popup.js';
+import Popup from './Popup.js';
 
 export default class PopupWithImage extends Popup {
-    constructor(popupSelector) {
-        super(popupSelector);
-    }
 
     open({ link, name }) {
-        this._popupElement.querySelector('.modal__input_image-link)').src = link;
+        const modalImageLink = this._popupElement.querySelector('.modal__input_image-link');
+
+        modalImageLink.src = link;
+        modalImageLink.alt = alt;
         this._popupElement.querySelector('.modal__input_caption').textContent = name;
 
         super.open();
