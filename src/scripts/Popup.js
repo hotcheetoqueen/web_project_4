e695ee0f0efd2e3c38f6c9bbf02448cecf2e8189
HCQPersonal <personalz.hcq@gmail.com>
2020-07-30 18:05:03 -0700

    refactor/tighten up close and overlay ref in Popup

--- src/scripts/Popup.js
+++ src/scripts/Popup.js
@@ -3,26 +3,19 @@ export default class Popup {
         this._popupSelector = popupSelector;
         this._popupElement = document.querySelector(this._popupSelector);
         this._popupElementClass = `${popupSelector.split("_")[0]}`.slice(1);
-        this._popupOverlay = document.querySelector('.overlay');
-        // this._popupImage = document.querySelector('.card-popup__figure');
 
-        this._closeButton = document.querySelector('.modal__close');
-        this._closeImgButton = document.querySelector('.card-popup__close');
+        this._closeButton = this._popupElement.querySelector('.modal__close');
         this._handleEscClose = this._handleEscClose.bind(this);
     }
 
         open() {
             this._popupElement.classList.add(`${this._popupElementClass}_visible`);
-            this._popupOverlay.classList.add('overlay_visible');
-            // this._popupImage.classList.add('card-popup__figure_visible');
 
             document.addEventListener('keyup', this._handleEscClose);
         }
 
         close() {
             this._popupElement.classList.remove(`${this._popupElementClass}_visible`);
-            this._popupOverlay.classList.remove('overlay_visible');
-            // this._popupImage.classList.remove('card-popup__figure_visible');
         
             document.removeEventListener('keyup', this._handleEscClose);
         }
@@ -38,14 +31,10 @@ export default class Popup {
               this.close();
             });
 
-            this._closeImgButton.addEventListener("click", () => {
+            this._popupElement.addEventListener("click", (e) => {
+              if(!e.target.closest('.modal__container')) {
                 this.close();
-              });
-
-            this._popupOverlay.addEventListener("click", () => {
-              this.close();
+              }
             });
-
-            // this._handleEscClose();
-          }
+        }
 }
